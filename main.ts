c99688f685ff6e247b3d0dcb96d2d5b8d16b56c9
Usman <usmanpasha18@gmail.com>
2024-01-05 07:29:33 -0800

    updated assignment complete

--- main.ts
+++ main.ts
@@ -490,4 +490,314 @@
 //     }
 // }
 
+// 31. No Users: Add an if test to Exercise 30 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
 
+// non empty array
+// let userNames:string[] = ["admin","mustafa","anika","eisha","bubbi","gia"];
+// function greetuser(username:string[]):void{
+//     if(username.length === 0){
+//         console.log("we need to find some users")
+//         return
+//     }
+// }
+// for (let i of userNames){
+//     if(i.toLowerCase()==="admin"){
+//         console.log("Hello admin, would you like to see status report ?")
+//     }else{
+//         console.log(`Hello ${i}, thank you for logging in again`)
+//     }
+// }
+// console.log("for non empty usernames :\n")
+// greetuser(userNames)
+// console.log("\nFor empty usernames\n")
+// userNames=[];
+// greetuser(userNames)
+
+
+// 32. Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+// • Make a list of five or more usernames called current_users.
+// • Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+// • Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a
+// new username. If a username has not been used, print a message saying that the username is available.
+// • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
+
+// const current_users:string[] = ["usman","anika","mustafa","eisha","ashley"];
+// let new_users:string[] = ["ahmed","usman","mustafa","aslam","akram"];
+// function checkuserName(current_users:string[],new_users:string[]):void{
+//     const lowercaseCurrentuser = current_users.map(user => user.toLowerCase())
+//     for(const newUser of new_users){
+//         const lowercaseNewuser = newUser.toLowerCase();
+//         if(lowercaseCurrentuser.includes(lowercaseNewuser)){
+//             console.log(`username "${newUser}", has already taken.Enter a new username`)
+//         }else{
+//             console.log(`username "${newUser}", is available`)
+//         }
+//     }
+// }
+// checkuserName(current_users,new_users);
+
+
+// 33. Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+// • Store the numbers 1 through 9 in a array.
+// • Loop through the array.
+// • Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th
+// 7th 8th 9th", and each result should be on a separate line.
+
+// const num:number[] = [1,2,3,4,5,6,7,8,9];
+// for(let i of num){
+//     let ordinal:string;
+//     if(i === 1){
+//         ordinal = "st";
+//     }else if(i === 2){
+//         ordinal = "nd";
+//     }else if(i === 3){
+//         ordinal = "rd";
+//     }else{ordinal = "th"}
+//     console.log(`${i}${ordinal}`)
+// }
+
+
+// 34. Pizzas: Think of at least three kinds of your favorite pizza. Store these
+// pizza names in a array, and then use a for loop to print the name of each pizza.
+// • Modify your for loop to print a sentence using the name of the pizza
+// instead of printing just the name of the pizza. For each pizza you should
+// have one line of output containing a simple statement like I like pepperoni
+// pizza.
+// • Add a line at the end of your program, outside the for loop, that states
+// how much you like pizza. The output should consist of three or more lines
+// about the kinds of pizza you like and then an additional sentence, such as
+// I really love pizza!
+
+// let pizzaName:string[] = ["chicken tikka pizza","peproni pizza","cheese pizza"];
+// for (let i = 0; i < pizzaName.length; i++){
+//     console.log(pizzaName[i]);
+// }
+// console.log("\n")
+// for(let i = 0; i < pizzaName.length; i++){
+//     console.log("I like "+pizzaName[i]+".");
+// }
+// console.log("\n")
+// console.log("I really like chicken tikka pizza.  ")
+// console.log("I love  peprori pizza flavour.  ")
+// console.log("Cheese pizza is most delicious then other.  ")
+
+
+// 35. Animals: Think of at least three different animals that have a common characteristic.
+// Store the names of these animals in a list, and then use a for loop to
+// print out the name of each animal.
+// • Modify your program to print a statement about each animal, such as
+// A dog would make a great pet.
+// • Add a line at the end of your program stating what these animals have in
+// common. You could print a sentence such as Any of these animals would
+// make a great pet!
+
+// let animal = ["dog","cat","parrot"];
+// for(let i=0; i < animal.length; i++){
+//     console.log(animal[i]);
+// }
+// for(let i=0; i <animal.length; i++){
+//     if(animal[i] === "dog"){
+//         console.log(`A ${animal[i]} is a loyal pet.`);
+//     }else if(animal[i] === "cat"){
+//         console.log(`A ${animal[i]} is a loving pet.`);
+//     }else if(animal[i] === "parrot"){
+//         console.log(`A ${animal[i]} is noisy pet. never make you boring.`)
+//     }else{console.log("No pet found")}
+// }
+
+
+// 36. T-Shirt: Write a function called make_shirt() that accepts a size and the
+// text of a message that should be printed on the shirt. The function should print
+// a sentence summarizing the size of the shirt and the message printed on it.
+// Call the function.
+
+// function make_shirt(size:string, message:string){
+//     console.log(`${size} ${message}`)
+//     return `${size} ${message}`
+// }
+// let result = make_shirt("large", " It is available")
+// console.log(result)
+
+
+// 37. Large Shirts: Modify the make_shirt() function so that shirts are large
+// by default with a message that reads I love TypeScript. Make a large shirt and a
+// medium shirt with the default message, and a shirt of any size with a different
+// message.
+
+// function make_shirt(size:string="Large", message:string="i love typescript."){
+//     console.log(`${size} , ${message}`)
+//     return `${size} ${message}`
+// }
+// make_shirt()
+// make_shirt("Medium shirt")
+// make_shirt("small size","only small size available")
+
+
+// 38. Cities: Write a function called describe_city() that accepts the name of
+// a city and its country. The function should print a simple sentence, such as
+// Karachi is in Pakistan. Give the parameter for the country a default value.
+// Call your function for three different cities, at least one of which is not in the
+// default country.
+
+// function describe_city(city:string, country:string = "unknown country"){
+//     console.log(`${city} is the city of ${country}`);
+// }
+// (describe_city("Karchi", "Pakistan"));
+// describe_city("Bejing", "China");
+// describe_city("Texas");
+
+
+// 39. City Names: Write a function called city_country() that takes in the name
+// of a city and its country. The function should return a string formatted like this:
+// "Lahore, Pakistan"
+// Call your function with at least three city-country pairs, and print the value
+// that’s returned.
+
+// function city_country(city:string, country:string):string
+// {
+//     // console.log(`${city},${country}`)
+//     return `"${city},${country}"`
+// }
+// console.log(city_country("Bejing","China"));
+// console.log(city_country("Tehran","Iran"));
+// console.log(city_country("karachi","pakistan"));
+
+
+// 40. Album: Write a function called make_album() that builds a Object
+// describing a music album. The function should take in an artist name and an
+// album title, and it should return a Object containing these two pieces of
+// information. Use the function to make three dictionaries representing different
+// albums. Print each return value to show that Objects are storing the
+// album information correctly.
+// Add an optional parameter to make_album() that allows you to store the
+// number of tracks on an album. If the calling line includes a value for the number
+// of tracks, add that value to the album’s Object. Make at least one new
+// function call that includes the number of tracks on an album.
+
+// function make_album(artist:string, title:string, track?:number):{artist:string, title:string, track?:number}
+// {
+//     const album ={
+//         artist,
+//         title,
+//         track,
+//     }
+//     if(album !== undefined){
+//         album.track = track;
+//     }
+//     return album;
+// }
+// const album1 = make_album("Ibrar ul Haq","Billo De Ghar");
+// const album2 = make_album("Atif Aslam", "Tery Bin", 12);
+// const album3 = make_album("Ali Azmat", "Garaj Baras Saawin", 3);
+// console.log(album1)
+// console.log(album2)
+// console.log(album3)
+
+
+// 41. Magicians: Make a array of magician’s names. Pass the array to a function
+// called show_magicians(), which prints the name of each magician in the array.
+
+// let magicianName = ["Harry Houdini","Criss Angel","David Coperfield","Derren Brown","Ricky Jay","Dynamo"];
+// function show_magicians(items:string[]):void{
+//     for( let i of items){
+//         console.log(i)
+//     }
+// }
+// show_magicians(magicianName);  
+// // solved by for loop
+// for (let i = 0; i < magicianName.length; i++){
+//     console.log(magicianName[i]);
+// }
+
+
+// 42. Great Magicians: Start with a copy of your program from Exercise 41.
+//  Write a function called make_great() that modifies the array of magicians by adding
+// the phrase the Great to each magician’s name. Call show_magicians() to
+// see that the list has actually been modified.
+
+// let magicianName = ["Harry Houdini","Criss Angel","David Coperfield","Derren Brown","Ricky Jay","Dynamo"];
+// function show_magicians(items:string[]):void
+// {
+//     for(let i of items){
+//         console.log(i)
+//     }
+
+// }
+// // show_magicians(magicianName);
+// function make_great(magicianName:string[]):string[]{
+//     let GreatMagician:string[] = magicianName.map(magicianName =>`The Great ${magicianName}`);
+//     return GreatMagician;
+// }
+// const GreatMagicianNames:string[] = make_great(magicianName);
+// show_magicians(magicianName);
+// show_magicians(GreatMagicianNames);
+
+
+// 43. Unchanged Magicians: Start with your work from Exercise 40. Call the
+// function make_great() with a copy of the array of magicians’ names. Because the
+// original array will be unchanged, return the new array and store it in a separate array.
+// Call show_magicians() with each array to show that you have one array of the original
+// names and one array with the Great added to each magician’s name.
+// const magiciansNames:string[]= ["Harry Houdini","Criss Angel","David Coperfield","Derren Brown","Ricky Jay","Dynamo"];   
+// function showMagicians(magiciansNames:string[]):void
+// {
+//     for(let item of magiciansNames){
+//         console.log(item);
+//     }
+// }
+
+// function make_great(magiciansNames:string[]): string[] {
+//     const GreatMagictian:string[] = magiciansNames.map(magiciansNames=> 
+//         `The Great ${magiciansNames}`);
+//         return GreatMagictian;
+// }
+// const GreatMagictianNames:string[]=make_great(magiciansNames);
+// showMagicians(magiciansNames);
+// showMagicians(GreatMagictianNames);
+
+
+// 44. Sandwiches: Write a function that accepts a array of items a person wants
+// on a sandwich. The function should have one parameter that collects as many
+// items as the function call provides, and it should print a summary of the sandwich
+// that is being ordered. Call the function three times, using a different number
+// // of arguments each time.
+
+// function Sandwiches(...items:string[]):void{
+//     console.log("Sandwich Summary:");
+//     if(items.length === 0){
+//         console.log("You order nothing. please add some items.");
+//     }else{
+//         items.forEach((item,index)=>{
+//             console.log(` ${index +1}. ${item}`);
+//         });
+//     }
+//     console.log("\n");
+// }
+//call function with different numbers of arguments
+// Sandwiches("BBQ", "Chicken Tikka", "Mayonaise","Cheesy");
+// Sandwiches("Egg Sandwich", "Club Sandwich");
+// Sandwiches("Honey","Tandori", "Cheese","Beef");
+// Sandwiches();
+
+// 45. Cars: Write a function that stores information about a car in a Object.
+// The function should always receive a manufacturer and a model name. It
+// should then accept an arbitrary number of keyword arguments. Call the function
+// with the required information and two other name-value pairs, such as a
+// color or an optional feature. 
+// Print the Object that’s returned to make sure all the information was
+// stored correctly.
+// function createCar(manufacturer:string, model:number, ...properties:[string,any][]):any{
+//     const car:any = {
+//         manufacturer,
+//         model,
+//     };
+//     for( const [key,value] of properties){
+//         car[key] = value;
+//     }
+//     return car;
+// }
+// //example usage
+// const myCar:string = createCar("Toyota Corolla", 2023, ["color", "red"],["optional Feature","Sunroof"],["Speed", "250 KM/hours"]);
+// console.log(myCar);
